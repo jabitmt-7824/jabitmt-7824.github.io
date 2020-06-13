dc3d931233f33c6e62bfd6e95ddfc94f83b124bc
Jabir MT <jabirmt7824@gmail.com>
2020-06-13 18:18:12 +0530

    add,deletion and count tasks

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
 var input=document.getElementById("input1");
 var addbutton=document.getElementById("add-button");
 var lists=document.getElementById("lists");
+var count=document.querySelector("#task-left span");
+var taskList = [];
 
+// key press handler in task input 
 input.addEventListener("keypress",function(){
 	addbutton.style.display="block";
 });
 
-function createItem(itemvalue){
-	 		var listItem=document.createElement('li');
-	 		var leftpart=document.createElement('div');
-	 		var checkBox = document.createElement("input");
-	 		var label = document.createElement("label");
-	 		var deleteButton = document.createElement("button");
-	 		deleteButton.classList.add("delete-btn");
-	 		listItem.classList.add("lists");
+// delete task from list
+function deleteTask (taskId) {
+	console.log("enter into deleteTask");
+    const updatedTaskList = taskList.filter(function(task){return task.id != taskId});
+    taskList = updatedTaskList;
+    renderList();
+    counter();
+ }
 
-	 		checkBox.type = "checkBox";
-	 		label.innerText=itemvalue;
-	 		deleteButton.innerText="Delete";
+// create list item in list container
+function createTaskItem (task) {
+    const listItem = document.createElement('li');
+    listItem.classList.add("lists");
 
-	 		leftpart.appendChild(checkBox);
-	 		leftpart.appendChild(label);
-	 		listItem.appendChild(leftpart);
-	 		listItem.appendChild(deleteButton);
-	 		lists.appendChild(listItem);
-	 	}
+    listItem.innerHTML = `<div> <input type="checkbox" id="${task.id}"} > <label for="${task.id}">${task.text}</label> </div> 
+    <button class="delete-btn" onclick="deleteTask(${task.id})">Delete</button>`;
+    lists.appendChild(listItem);
+ }
 
+// render or display list items
+function renderList (){
+    lists.innerHTML = '';
+
+    for (let i = 0; i < taskList.length; i++) {
+      createTaskItem(taskList[i]);
+    }
+}
+
+// add task to list
+function addTask(task){
+	if (task) {
+      taskList.push(task);
+      renderList();
+      return;
+    }
+}
+
+// display number of tasks in list
+function counter (){
+	count.innerText=taskList.length;
+}
+
+// add button event handler
 addbutton.addEventListener("click",function(){	
-	let v=input.value;
-	createItem(v);
+	let text=input.value;
+	const task = {text, id: Date.now().toString()};
+	addTask(task);
 	input.value="";
 	addbutton.style.display="none";
-	let items=document.querySelectorAll("#lists li");
-	let count=document.querySelector("#task-left span");
-	count.innerText=items.length;
-
+	counter();
 });
+
