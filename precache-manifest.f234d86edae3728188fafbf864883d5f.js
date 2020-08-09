a6f77eab9d4fcce35c73a8114c52d88bd65c8f36
Jabir MT <jabirmt7824@gmail.com>
2020-08-10 00:09:40 +0530

    Updates

---
+++ precache-manifest.f234d86edae3728188fafbf864883d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e72f3e342213fb99b4a674791657b11",
+    "url": "/tooltip/index.html"
+  },
+  {
+    "revision": "43b96c70dbc428ea389b",
+    "url": "/tooltip/static/css/main.3b26859d.chunk.css"
+  },
+  {
+    "revision": "eb0daefde46ad1fa53fb",
+    "url": "/tooltip/static/js/2.c396f6f1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tooltip/static/js/2.c396f6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b96c70dbc428ea389b",
+    "url": "/tooltip/static/js/main.0a1b9832.chunk.js"
+  },
+  {
+    "revision": "3784c8f55818a854f3b2",
+    "url": "/tooltip/static/js/runtime-main.14c2197d.js"
+  }
+]);
